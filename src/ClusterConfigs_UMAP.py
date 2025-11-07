716778c5ece07db5c7da7d71089c38f6ab0b5c2a
Roman Baravalle <romanbaravalle@gmail.com>
2025-11-06 21:10:37 -0600

    add umap batch

---
+++ src/ClusterConfigs_UMAP.py
@@ -0,0 +1,29 @@
+CONFIG_EXPANSE_CPU = """
+# >>> Conda setup
+source ~/.bashrc
+module purge
+conda activate NewBatchtk
+# <<< End Conda setup
+
+# Load modules
+echo "Loading modules..."
+module load openmpi/mlnx/gcc/64/4.1.5a1
+         
+# Add project root and src to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$PWD
+export PYTHONPATH=$PYTHONPATH:$PWD/src
+
+time mpirun -n $SLURM_NTASKS nrniv -mpi -python src/init_UMAP.py
+"""
+
+slurm_args = {
+    'allocation': 'TG-MED240050', # 'TG-IBN140002' 'TG-MED240058' 'TG-MED240050'
+    'realtime': '10:30:00',
+    'nodes': '1',
+    'coresPerNode': '120',
+    'mem': '240G',
+    'partition': 'compute',
+    'email': 'romanbaravalle@gmail.com',
+    'custom': '',
+    'command': CONFIG_EXPANSE_CPU,  # ← FIXED: remove braces
+}
