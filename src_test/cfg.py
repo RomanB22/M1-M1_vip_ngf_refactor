5132f65a7b22b8e542ed6ffa6baab7b9ae257748
Roman Baravalle <romanbaravalle@gmail.com>
2026-03-06 10:09:27 -0600

    update cell diversity

--- src_test/cfg.py
+++ src_test/cfg.py
@@ -27,7 +27,7 @@ cfg._batchtk_path_pointer = None
 # Run parameters
 #------------------------------------------------------------------------------
 cfg.diversity = True
-cfg.percentage = 2  # std of variation in somatic passive properties in the populations
+cfg.pasUniformFrac = 0.30   # +/-10% variation in somatic passive properties in the populations
 
 cfg.preTone = 500
 cfg.postTone = 1500 # Movement part
@@ -304,7 +304,7 @@ cfg.distributeSynsUniformly = True
 cfg.layer = {'1':[0.0, 0.1], '2': [0.1,0.29], '4': [0.29,0.37], '5A': [0.37,0.47], '24':[0.1,0.37], '5B': [0.47,0.8], '6': [0.8,1.0], 
 'longTPO': [2.0,2.1], 'longTVL': [2.1,2.2], 'longS1': [2.2,2.3], 'longS2': [2.3,2.4], 'longcM1': [2.4,2.5], 'longM2': [2.5,2.6], 'longOC': [2.6,2.7]}  # normalized layer boundaries
 
-cfg.singleCellPops = True  # Create pops with 1 single cell (to debug)
+cfg.singleCellPops = False  # Create pops with 1 single cell (to debug)
 cfg.weightNorm = 1  # use weight normalization
 cfg.weightNormThreshold = 4.0  # weight normalization factor threshold
 
--- src_test/netParams.py
+++ src_test/netParams.py
@@ -175,12 +175,12 @@ def parse_cfg_mutations(cfg):
         out.append(Mutation(**m))  # dataclass validates keys
     return out
 
-# # Apply external Ca concentration to all sections that define ca ions
-# for cell_name, rule in netParams.cellParams.items():
-#     for sec_name, sec_def in rule.get('secs', {}).items():
-#         ions = sec_def.get('ions', {})
-#         if 'ca' in ions:
-#             ions['ca']['o'] = cfg.cao_secs
+# Apply external Ca concentration to all sections that define ca ions
+for cell_name, rule in netParams.cellParams.items():
+    for sec_name, sec_def in rule.get('secs', {}).items():
+        ions = sec_def.get('ions', {})
+        if 'ca' in ions:
+            ions['ca']['o'] = cfg.cao_secs
 
 if getattr(cfg, "mutations_enabled", True):
     muts = parse_cfg_mutations(cfg)
@@ -202,10 +202,12 @@ if cfg.drugTreatment:
             print("[DRUG]", line)
 
 if cfg.diversity:
+    low = 1.0 - cfg.pasUniformFrac
+    high = 1.0 + cfg.pasUniformFrac
     for cellType in netParams.cellParams.keys():
         for param in netParams.cellParams[cellType]['secs']['soma']['mechs']['pas']:
             value = netParams.cellParams[cellType]['secs']['soma']['mechs']['pas'][param]
-            netParams.cellParams[cellType]['secs']['soma']['mechs']['pas'][param] = f'{value}*normal(1, {cfg.percentage})'
+            netParams.cellParams[cellType]['secs']['soma']['mechs']['pas'][param] = f'{value}*uniform({low}, {high})'
 
 #------------------------------------------------------------------------------
 # Population parameters
