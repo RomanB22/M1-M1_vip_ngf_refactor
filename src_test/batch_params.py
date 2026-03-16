c3c802c6df9839a5029a05c7bea1eb409283bb8f
Roman Baravalle <romanbaravalle@gmail.com>
2026-03-16 13:50:58 -0500

    update batchParams

--- src_test/batch_params.py
+++ src_test/batch_params.py
@@ -7,16 +7,16 @@ def get_batch_params(minChg, maxChg):
         'weightLong.cM1': (0.1 * minChg, 0.8 * maxChg),
         'weightLong.M2': (0.1 * minChg, 0.8 * maxChg),
         'weightLong.OC': (0.1 * minChg, 0.8 * maxChg),
-        'EEGain': (1.0 * minChg, 1.0 * maxChg),
-        'IEweights.0': (1.0 * minChg, 1.0 * maxChg),  # L2/3+4
-        'IEweights.1': (1.0 * minChg, 1.0 * maxChg),  # L5
-        'IEweights.2': (1.0 * minChg, 1.0 * maxChg),  # L6
-        'IIweights.0': (1.0 * minChg, 1.0 * maxChg),  # L2/3+4
-        'IIweights.1': (1.0 * minChg, 1.0 * maxChg),  # L5
-        'IIweights.2': (1.0 * minChg, 1.0 * maxChg),  # L6
-        'EICellTypeGain.PV': (1.0 * minChg, 2.0 * maxChg),
-        'EICellTypeGain.SOM': (1.0 * minChg, 2.0 * maxChg),
-        'EICellTypeGain.VIP': (1.0 * minChg, 2.0 * maxChg),
-        'EICellTypeGain.NGF': (1.0 * minChg, 2.0 * maxChg),
+        'EEGain': (0.5 * minChg, 4.0 * maxChg),
+        'IEweights.0': (0.5 * minChg, 2.0 * maxChg),  # L2/3+4
+        'IEweights.1': (0.5 * minChg, 2.0 * maxChg),  # L5
+        'IEweights.2': (0.5 * minChg, 2.0 * maxChg),  # L6
+        'IIweights.0': (0.5 * minChg, 2.0 * maxChg),  # L2/3+4
+        'IIweights.1': (0.5 * minChg, 2.0 * maxChg),  # L5
+        'IIweights.2': (0.5 * minChg, 2.0 * maxChg),  # L6
+        'EICellTypeGain.PV': (1.0 * minChg, 4.0 * maxChg),
+        'EICellTypeGain.SOM': (1.0 * minChg, 4.0 * maxChg),
+        'EICellTypeGain.VIP': (1.0 * minChg, 4.0 * maxChg),
+        'EICellTypeGain.NGF': (1.0 * minChg, 4.0 * maxChg),
         # 'scaleDensity': (0.15)
     }
