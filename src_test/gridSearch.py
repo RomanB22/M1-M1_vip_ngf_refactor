477cb3d0fa123871f58f183a1f699de6729054a8
Roman Baravalle <romanbaravalle@gmail.com>
2026-04-24 11:57:42 -0500

    Added channelopathies solutions

---
+++ src_test/gridSearch.py
@@ -0,0 +1,28 @@
+from netpyne.batchtools.search import search, grid
+
+# -------------------------------------------------------------------------
+# Batch parameters – grid search over simConfig.seeds sub-keys
+# -------------------------------------------------------------------------
+params = {
+    'seeds.conn': [1, 2, 3],
+    'seeds.stim': [1, 2, 3],
+    'seeds.loc':  [1, 2, 3],
+}
+
+# -------------------------------------------------------------------------
+# Run the grid search (27 combinations: 3 x 3 x 3)
+# -------------------------------------------------------------------------
+search(
+    job_type='suny',      # change to 'hpc_slurm' / 'sge' for cluster
+    comm_type = 'sfs', # 'socket', 'sfs', None
+    params=params,
+    run_config={
+        'command': 'conda activate CompNeuroCourse \nexport UCX_TLS=tcp,self \nexport LD_LIBRARY_PATH=~/miniconda3/envs/CompNeuroCourse/lib\nsrun --mpi=pmi2 nrniv -python -mpi python -u src/init.py',
+        'cores': 52,
+        'mem': '200G',
+        'realtime': '10:30:00'
+    },
+    label='gridSearch_seeds',
+    output_path='./batch_results',
+    checkpoint_path='./batch_checkpoints',
+)
